4e3566aaf3cfe0727517da87415caf846bc31472
Nina Raganová <57150586+Ninush2498@users.noreply.github.com>
2019-11-20 21:31:48 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -6,7 +6,6 @@
 package game;
 
 import java.util.Scanner;
-import javafx.util.Pair;
 
 /**
  *
@@ -17,8 +16,18 @@ public class Game {
     static int index = -1;
     static int count = 0;
     
-    public static Pair<Integer, Integer> nextTurn(Player[] p, Pair<Integer, Integer> guess) {
-        if (guess.getKey()==1 && guess.getValue()==0) {
+    public static boolean nextBoolean() {
+        Scanner s = new Scanner(System.in);
+        return s.nextBoolean();
+    }
+    
+    public static int nextInteger() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+    
+    public static Guess nextTurn(Player[] p, Guess guess) {
+        if (guess.count()==1 && guess.value()==0) {
             return newGame(p, guess);
         } else {
             if (index<p.length-1) {
@@ -27,7 +36,6 @@ public class Game {
                 index = 0;
             }
             if (p[index].isActive()) {
-                System.out.println("Player " + (index+1));
                 return p[index].play(p, guess, index);
             } else {
                 return guess;
@@ -35,7 +43,22 @@ public class Game {
         }
     }
     
-    public static Pair<Integer, Integer> newGame(Player[] p, Pair<Integer, Integer> guess) {
+    public static void winnerPrinter(int winner, Player[] p) {
+        System.out.println("Player " + (winner)); 
+        p[winner-1].win();
+    }
+    
+    public static void newRoundPrinter(Player[] p) {
+        System.out.println("New round");     
+        System.out.println("Player " + (index+1));
+        System.out.println("Your dices:");
+        for (int d : p[index].getDices()) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+    }
+    
+    public static Guess newGame(Player[] p, Guess guess) { //pozmenit
         int playing = 0;
         int winner = -1;
         for (int i=0; i<p.length; i++) {
@@ -45,25 +68,46 @@ public class Game {
             }            
         }
         
+        if (index==-1) {
+            int i = 1;
+            for (Player pl : p) {
+                pl.wannaRoll(i);
+                i++;
+            }
+        }
+        
         if (playing<2) {
-            System.out.println("Player " + (winner+1));
-            p[winner].win();
-            return new Pair(0,0);
+            winnerPrinter(winner+1, p);
+            return new Guess(0,0);
         } else {
-            System.out.println("New round");
             if (index<p.length-1) {
                 index++;
             } else {
                 index = 0;
             }
-            System.out.println("Player " + (index+1));
-            System.out.println("Your dices:");
-            for (Dice d : p[index].getDices()) {
-                System.out.print(d.getValue() + " ");
-            }
-            System.out.println();
+            newRoundPrinter(p);
+            p[index].askShow();
             return p[index].raise(guess);
         }
+    }
+    
+    public static Player[] createGame(int n, int m) {
+        Player[] players = new Player[n];        
+        for (int i=0; i<n; i++) {
+            players[i] = new Player(m);
+            count += m;
+        }
+        return players;
+    }
+    
+    public static void game(Player[] players, Guess guess) {
+        guess = newGame(players, guess);
+        while(true) {
+            guess = nextTurn(players, guess);
+            if (guess.count()==0) {
+                break;
+            }
+        }        
     }
     
 
@@ -72,14 +116,11 @@ public class Game {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner s = new Scanner(System.in);
-        Player[] players;
-        Pair<Integer, Integer> guess = new Pair(1,0);
         int n, m;
         
         System.out.println("Enter the number of players:");
         while(true) {
-            n = s.nextInt();
+            n = nextInteger();
             if (n<2) {
                 System.out.println("At least 2 players are required. Please try again.");
             } else {
@@ -89,7 +130,7 @@ public class Game {
         
         System.out.println("Enter the number of dices:");
         while(true) {
-            m = s.nextInt();
+            m = nextInteger();
             if (m<1) {
                 System.out.println("You need at least 1 dice to play. Please try again.");
             } else {
@@ -97,20 +138,8 @@ public class Game {
             }
         }
         
-        players = new Player[n];        
-        for (int i=0; i<n; i++) {
-            players[i] = new Player(m);
-            count += m;
-        }
-        
         System.out.println("Game starts");
-        guess = newGame(players, guess);
-        while(true) {
-            guess = nextTurn(players, guess);
-            if (guess.getKey()==0) {
-                break;
-            }
-        }
+        game(createGame(n, m), new Guess(1,0));
         System.out.println("Game ends");
     }
     
