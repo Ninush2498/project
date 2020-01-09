433fda62a35f51f05723ea79d4b0a3b0cd07a12f
Nina Raganová <57150586+Ninush2498@users.noreply.github.com>
2020-01-09 18:27:10 +0100

    Update http_server.js

--- game/http_server.js
+++ game/http_server.js
@@ -115,7 +115,7 @@ io.on('connection', (socket)=>{
                                 value: {
                                     message: "How many dices do you wanna show?",
                                     alert: "Sorry, you can't show that many dices. Try again.",
-                                    bigger: 0,
+                                    bigger: -1,
                                     smaller: players[num].getDices().length-players[num].visible,
                                     continue: "show"
                                 }
@@ -128,6 +128,110 @@ io.on('connection', (socket)=>{
                             socket.emit('newMessage', {
                                 code: 2,
                                 value: "OK, you don't have to."
+                            });
+                        }
+                        break;
+                    case "raiseOne":
+                        if (newMessage.value.answer) {
+                            socket.emit('newMessage', {
+                                code: 4,
+                                value: {
+                                    message: "Value of dices:",
+                                    alert: "",
+                                    bigger: "neg",
+                                    smaller: "pos",
+                                    args: {n: newMessage.value.args, one: true},
+                                    continue: "raise2"
+                                }
+                            });
+                        } else {
+                            socket.emit('newMessage', {
+                                code: 4,
+                                value: {
+                                    message: "Number of dices:",
+                                    alert: "",
+                                    bigger: "neg",
+                                    smaller: "pos",
+                                    args: null,
+                                    continue: "raise1"
+                                }
+                            });
+                        }
+                        break;
+                    case "raiseTwo":
+                        if (newMessage.value.answer) {
+                            guess = new Guess(newMessage.value.args.n, newMessage.value.num);
+                            socket.emit('newMessage', {
+                                code: 5,
+                                value: "raise"
+                            });
+                        } else {
+                            socket.emit('newMessage', {
+                                code: 4,
+                                value: {
+                                    message: "Value of dices:",
+                                    alert: "",
+                                    bigger: "neg",
+                                    smaller: "pos",
+                                    args: {n: newMessage.value.args.n, one: newMessage.value.args.one},
+                                    continue: "raise2"
+                                }
+                            });
+                        }
+                        break;
+                    case "play1":
+                        if (newMessage.value.answer) {
+                            if (Player.canRaise(guess, Game.count)) {
+                                players[num].askShow();
+                            } else {
+                                socket.emit('newMessage', {
+                                    code: 2,
+                                    value: "Sorry, you can't raise because there are no more dices."
+                                })
+                                var output = "Do you really think there ";
+                                if (guess.count==1) {
+                                    output += "isn't " + guess.count + " dice ";
+                                } else {
+                                    output += "aren't " + guess.count + " dices ";
+                                }
+                                output += "with value " + guess.value + "? (yes/no)";
+                                socket.emit('newMessage', {
+                                    code: 3,
+                                    value: {
+                                        message: output,
+                                        continue: "play2"
+                                    }
+                                });
+                            }
+                        } else {
+                            var output = "Do you really think there ";
+                            if (guess.count==1) {
+                                output += "isn't " + guess.count + " dice ";
+                            } else {
+                                output += "aren't " + guess.count + " dices ";
+                            }
+                            output += "with value " + guess.value + "? (yes/no)";
+                            socket.emit('newMessage', {
+                                code: 3,
+                                value: {
+                                    message: output,
+                                    continue: "play2"
+                                }
+                            });
+                        }
+                        break;
+                    case "play2":
+                        if (newMessage.value.answer) {
+                            var count = new Number(Game.count);
+                            players[num].stop(players, guess, Game.index, count);
+                        } else {
+                            socket.emit('newMessage', {
+                                code:3,
+                                value: {
+                                    message: "Do you wanna raise? (yes/no)",
+                                    continue: "play1",
+                                    args: {}
+                                }
                             });
                         }
                         break;
@@ -166,13 +270,154 @@ io.on('connection', (socket)=>{
                     case "show":
                         players[num].showDices(newMessage.value.num);
                         break;
+                    case "raise1":
+                        switch(Player.validNumberRaise(guess, newMessage.value.num, Game.count)) {
+                            case "count":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "Too much! Must be less than " + (Game.count+1) + ".",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: null,
+                                        continue: "raise1"
+                                    }
+                                });
+                                break;
+                            case "lessOnePossible":
+                                socket.emit('newMessage', {
+                                    code: 2,
+                                    value: "Too little! Must be at least " + (2*guess.count) + " of any value or " + (guess.count+1) + " dices of value 1."
+                                });
+                                socket.emit('newMessage', {
+                                    code: 3,
+                                    value: {
+                                        message: "So you can enter 1 as a value in the next step. Do you want to do that? (yes/no)",
+                                        continue: "raiseOne",
+                                        num: newMessage.value.num
+                                    }
+                                });
+                                break;
+                            case "lessOne":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "Too little! Must be at least " + (2*guess.count) + " of any value or " + (guess.count+1) + " dices of value 1.",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: null,
+                                        continue: "raise1"
+                                    }
+                                });
+                                break;
+                            case "lessPossible":
+                                socket.emit('newMessage', {
+                                    code: 2,
+                                    value: "Too little! Must be at least " + guess.count + " of any value or " + Math.floor(guess.count/2+1) + " dices of value 1."
+                                });
+                                socket.emit('newMessage', {
+                                    code: 3,
+                                    value: {
+                                        message: "So you can enter 1 as a value in the next step. Do you want to do that? (yes/no)",
+                                        continue: "raiseOne",
+                                        num: newMessage.value.num
+                                    }
+                                });
+                                break;
+                            case "less":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "Too little! Must be at least " + guess.count + " of any value or " + Math.floor(guess.count/2+1) + " dices of value 1.",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: null,
+                                        continue: "raise1"
+                                    }
+                                });
+                                break;
+                            case "valid":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "Value of dices:",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: {n: newMessage.value.num, one: false},
+                                        continue: "raise2"
+                                    }
+                                });
+                                break;
+                        }
+                        break;
+                    case "raise2":
+                        switch(Player.validValueRaise(guess, newMessage.value.args.n, newMessage.value.num, newMessage.value.args.one)) {
+                            case "range":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "How could that be on a dice? Try again.",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: {n: newMessage.value.args.n, one: newMessage.value.args.one},
+                                        continue: "raise2"
+                                    }
+                                });
+                                break;
+                            case "one":
+                                socket.emit('newMessage', {
+                                    code: 4,
+                                    value: {
+                                        message: "You promised to enter value 1, remember? Try again.",
+                                        alert: "",
+                                        bigger: "neg",
+                                        smaller: "pos",
+                                        args: {n: newMessage.value.args.n, one: newMessage.value.args.one},
+                                        continue: "raise2"
+                                    }
+                                });
+                                break;
+                            case "noRaise":
+                                socket.emit('newMessage', {
+                                    code: 2,
+                                    value: "You didn't raise the number, therefore you have to raise the value."
+                                });
+                                socket.emit('newMessage', {
+                                    code: 3,
+                                    value: {
+                                        message: "Do you want to change the number? (yes/no)",
+                                        continue: "raiseTwo",
+                                        num: newMessage.value.num
+                                    }
+                                });
+                                break;
+                            case "valid":
+                                guess = new Guess(newMessage.value.args.n, newMessage.value.num);
+                                socket.emit('newMessage', {
+                                    code: 5,
+                                    value: "raise"
+                                });
+                                break;
+                        }
+                        break;
                 }
                 break;
             case "update":
                 switch(newMessage.value) {
                     case "roll":
+                        var playing = 0;
+                        for (pl of players) {
+                            if (pl.isActive()) {
+                                playing++;
+                            }
+                        }
                         updated++;
-                        if (updated==n) {
+                        if (updated==playing) {
                             updated = 0;
                             Game.newGame2(players, clients);
                         }
@@ -186,7 +431,14 @@ io.on('connection', (socket)=>{
                             }
                             it++;
                         });
-                        players[num].raise(guess);
+                        players[num].raise();
+                        break;
+                    case "raise":
+                        Game.nextTurn(players, guess);
+                        break;
+                    case "play":
+                        guess = new Guess(1,0);
+                        Game.nextTurn(players, guess);
                         break;
                 }
                 break;
